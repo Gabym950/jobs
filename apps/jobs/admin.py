0f2af81436508d534715def133e90c8652e7291e
gabym <gmagallanes@psychcampus.com>
2026-04-03 00:20:01 -0300

    Register Job Admin

--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Job
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'external_id', 'status', 'salary', 'country')
+    list_filter = ('status', 'country')
+    search_fields = ('title', 'external_id')
+    ordering = ('-id',)
